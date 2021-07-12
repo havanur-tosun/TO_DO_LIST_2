8cb5e867242e4893b3469daca350f09cc2706ed9
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-07-12 13:23:29 -0400

    commit

--- app.js
+++ app.js
@@ -1,39 +1,85 @@
-const clear = document.querySelector(".clear");
+// const clear = document.querySelector(".clear");
 
-const dateElement = document.getElementById("date");
+// const dateElement = document.getElementById("date");
 
-const list = document.getElementById("list");
+
+
+
+
+const CHECK = "far fa-circle";
+const UNCHECK = "fas fa-check-circle";
+const LINE_THROUGH = "lineThrough";
+
+
+function addToDo(toDo, id, done, trash) {
+  if (trash) { return; }
+
+const DONE = done ? CHECK : UNCHECK;
+
+const LIST = done ? LINE_THROUGH : "";
+
+const text = 
+  `<li class="item">
+    <i class="far ${DONE}" aria-hidden="true" job="complete" id="${id}"></i>
+    <p class="text${LINE}"> ${toDo} </p>
+    <i class="far fa-trash-alt" aria-hidden="true" job="delete" id="${id}"></i>
+  </li>`
+
+const position = "beforeend";
+
+list.insertAdjacentHTML(position, text);
+}
+
 
 const input = documen.getElementById("input");
 
-const element = document.getElementById("element");
-
-element.insertAdjacentHTML(position, text);
-
-function addToDo(toDo) {
-  const text = 
-    `<li class="item">
-      <i class="far fa-circle" aria-hidden="true" job="complete"></i>
-      <p class="text">${toDo}</p>
-      <i class="far fa-trash-alt" aria-hidden="true" job="delete"></i>
-    </li>`
-  
-
-  const position = "beforeend";
-
-  list.insertAdjacentHTML(position, text);
-}
-
-addToDo("Drink Coffee");
-
-
-document.addEventListener("keyup", function(event){
-    if(event.keyCode == 13) {
-        const toDo = input.value;
-        if(toDo) {
-            addToDo(toDo);
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode == 13) {
+    const toDo = input.value;
+    if (toDo) {
+      AddToDo(toDo, id, false, false);
+      LIST.push(
+        {
+          name: toDo,
+          id: id,
+          done: false,
+          trash: false
         }
-        input.value = "";
+      )
     }
+    input.value = "";
+    id++;
+  }
+});
+
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  LIST[element.id].done = LIST[element.id].done ? false: true;
 }
 
+
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash = true;
+}
+
+const list = document.getElementById("list");
+
+list.addEventListener("click", function(event){
+  let element = event.target;  //  <i class="far fa-trash-alt" job="delete" id="0"></i>
+  const elementJOB = event.target.attributes.job.value; // delete or complete
+  if(elementJOB == "complete"){
+    completeToDo(element);
+  }
+  else if(elementJOB == "delete"){
+    removeToDo(        );
+  }
+});
+
+localStorage.setItem('key', 'value');
+
+let variable = localStorage.getItem("key");
+
+localStorage.setItem("ToDo", JSON.stringify(LIST));
